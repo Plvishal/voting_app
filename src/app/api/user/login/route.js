374211a3922c560d2e918bff03d37b7c1fd02d7e
Vishal <vishalpal1501@gmail.com>
2024-05-07 17:48:45 +0530

    generate  jwt token done and user login successfully done

--- src/app/api/user/login/route.js
+++ src/app/api/user/login/route.js
@@ -5,29 +5,46 @@ import jwt from 'jsonwebtoken';
 import { NextResponse } from 'next/server';
 ConnectDB();
 export async function POST(req, res) {
-  const { email, password } = await req.json();
-  const user = await User.findOne({ email });
-  if (!user) {
-    return NextResponse.json(
-      {
-        error: 'User does not exits',
-      },
-      {
-        status: 400,
-      }
-    );
+  try {
+    const { email, password } = await req.json();
+    const user = await User.findOne({ email });
+    if (!user) {
+      return NextResponse.json(
+        {
+          error: 'User does not exits',
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+    //   check password is valid or not
+    const validPaasword = await bcryptjs.compare(password, user.password);
+    if (!validPaasword) {
+      return NextResponse.json(
+        {
+          msg: 'Incorrect password',
+        },
+        { status: 400 }
+      );
+    }
+    // Generating JSON Web Token
+    const tokenData = {
+      id: user._id,
+      username: user.username,
+    };
+    const token = jwt.sign(tokenData, process.env.NEXT_PUBLIC_SECRET_KEY, {
+      expiresIn: '1h',
+    });
+    const response = NextResponse.json({
+      msg: 'Login successfully done',
+      success: true,
+    });
+    response.cookies.set('token', token, {
+      httpOnly: true,
+    });
+    return response;
+  } catch (error) {
+    return NextResponse.json({ error: error.message });
   }
-  //   check password is valid or not
-  const validPaasword = await bcryptjs.compare(password, user.password);
-  if (!validPaasword) {
-    return NextResponse.json(
-      {
-        msg: 'Incorrect password',
-      },
-      { status: 400 }
-    );
-  }
-  
-
-  return NextResponse.json('ok');
 }
